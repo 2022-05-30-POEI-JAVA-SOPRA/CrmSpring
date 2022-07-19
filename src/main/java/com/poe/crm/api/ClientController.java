6022b57bc0d813dc7bdd2b92c79b91840bb0c020
jcdominguez <jcd.pro@gmail.com>
2022-07-19 15:11:29 +0200

    ajout service GET 1 Client

--- src/main/java/com/poe/crm/api/ClientController.java
+++ src/main/java/com/poe/crm/api/ClientController.java
@@ -3,13 +3,17 @@ package com.poe.crm.api;
 import com.poe.crm.business.Client;
 import com.poe.crm.business.service.CrmService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
+@RequestMapping("api")
 public class ClientController {
 
     @Autowired
@@ -24,6 +28,17 @@ public class ClientController {
     @PostMapping("clients")
     public void createClient(@RequestBody Client client){
         crmService.addClient(client);
+    }
+
+    @GetMapping("clients/{id}")
+    public ResponseEntity<Client> findClientById(@PathVariable("id") Long id){
+        Optional<Client> o = crmService.findClient(id);
+        if(o.isPresent()){
+            return ResponseEntity.status(HttpStatus.OK).body(o.get());
+        }
+        else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
     }
 
 

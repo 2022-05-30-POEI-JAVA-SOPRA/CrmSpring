53fd5c29c9f1b323488b2d88a929cae5fa92235b
jcdominguez <jcd.pro@gmail.com>
2022-07-21 09:58:33 +0200

    transformation du Service Client pour utiliser la BD à travers le Repository JPA

--- src/main/java/com/poe/crm/business/service/CrmService.java
+++ src/main/java/com/poe/crm/business/service/CrmService.java
@@ -5,69 +5,42 @@ import com.poe.crm.dao.ClientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Optional;
 
 @Service
 public class CrmService {
 
-    ArrayList<Client> clients = new ArrayList<>();
-    long index = 0;
-
     @Autowired
     ClientRepository clientRepository;
 
     public List<Client> getAllClients() {
-        return clients;
+        return clientRepository.findAll();
     }
 
     public void addClient(Client client){
-        index++;
-        client.setId(index);
-        clients.add(client);
+       clientRepository.save(client);
     }
 
     public Optional<Client> findClient(Long id){
-        for(Client client : clients){
-            if(client.getId().equals(id)){
-                return Optional.of(client);
-            }
-        }
-        return Optional.empty();
+        return clientRepository.findById(id);
     }
 
     public boolean deleteClient(Long id){
-        Iterator<Client> it = clients.iterator();
-        while(it.hasNext()){
-            Client client = it.next();
-            if(client.getId().equals(id)) {
-                it.remove();
-                return true;
-
-                //break;  // Continue la boucle pour tester l'accès concurrentiel
-            }
+        if(clientRepository.existsById(id)) {
+            clientRepository.deleteById(id);
+            return true;
+        } else {
+            return false;
         }
-        return false;
-
-//        for(Client client: clients){
-//            if(client.getId().equals(id)){
-//                clients.remove(client);
-//                break;
-//            }
-//        }
     }
 
     public boolean updateClient(Client client){
-
-        for(Client c: clients){
-            if(c.getId().equals(client.getId())){
-                clients.remove(c);
-                clients.add(client);
-                return true;
-            }
+        if(clientRepository.existsById(client.getId())) {
+            clientRepository.save(client);
+            return true;
+        } else {
+            return false;
         }
-        return false;
     }
 }
--- src/main/java/com/poe/crm/dao/ClientRepository.java
+++ src/main/java/com/poe/crm/dao/ClientRepository.java
@@ -4,9 +4,8 @@ import com.poe.crm.business.Client;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+
 @Repository
 public interface ClientRepository extends JpaRepository<Client, Long> {
-
-
 
 }
